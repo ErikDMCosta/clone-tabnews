6c2eb10d8f97fba2fd743e1363f50d3f85dfb117
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-04-02 19:43:00 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,14 @@
 // /api/v1/status
+import database from "../../../../infra/database.js";
 
-function status(request, response) {
+async function status(request, response) {
   // SEM FORMATO
   // response.status(200).send("alunos do curso.dev são pessoas acima da média");
   // UTF-8
+  // console.log(database);
+  const result = await database.query('SELECT 1 + 1;');
+  console.log(result.rows);
   response.status(200).json({ chave: "valor - são pessoas acima da média" });
 }
 
 export default status;
- 
