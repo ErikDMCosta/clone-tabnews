8fd936f1adf156e9fd50aff96ea5cce85bc85b12
Érik de Moraes Costa <erikdmcosta@protonmail.com>
2024-05-04 18:31:31 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -6,9 +6,63 @@ async function status(request, response) {
   // response.status(200).send("alunos do curso.dev são pessoas acima da média");
   // UTF-8
   // console.log(database);
-  const result = await database.query('SELECT 1 + 1;');
-  console.log(result.rows);
-  response.status(200).json({ chave: "valor - são pessoas acima da média" });
+  // const result = await database.query('SELECT 1 + 1;');
+  // console.log(result.rows);
+  // response.status(200).json({ chave: "valor - são pessoas acima da média" });
+
+  // response.status(200).json({});
+
+  // const updatedAt = Date.now();
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseConnectionResult.rows[0].max_connections;
+  // console.log(databaseVersionResult);
+
+  // const databaseOpenedConnectionsResult = await database.query("SELECT * FROM pg_stat_activity;");
+
+  // const databaseName = "local_db";
+  // const databaseName = request.query.databaseName;
+  const databaseName = process.env.POSTGRES_DB;
+  // console.log(`Banco de Dados selecionado: ${databaseName}`);
+
+  // const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='local_db';");
+  // const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='" + databaseName + "';");
+  // const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='';");
+  // const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname='';';");
+  // const databaseOpenedConnectionsResult = await database.query("SELECT count(*)::int FROM pg_stat_activity WHERE datname=''; SELECT pg_local(4); --';");
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+    values: [databaseName],
+    // `"SELECT count(*)::int FROM pg_stat_activity WHERE datname='${databaseName}';"`,
+  }); // Placeholder ${}
+
+  // console.log(databaseOpenedConnectionsResult.rows);
+  // console.log(databaseOpenedConnectionsResult.rows.length);
+  // const databaseOpenedConnectionValue = databaseOpenedConnectionsResult.rows.length;
+  const databaseOpenedConnectionValue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  // console.log(databaseOpenedConnectionValue);
+
+  response.status(200).json({
+    // updated_at: "textomaluco",
+    // updated_at: null,
+    updated_at: updatedAt,
+    dependences: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,5 +1,25 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   // console.log(response);
+
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  // console.log(responseBody);
+  // expect(responseBody.updated_at).toBeDefined();
+
+  const parseUpdateAt = new Date(responseBody.updated_at).toISOString();
+  // console.log(parseUpdateAt);
+  expect(responseBody.updated_at).toEqual(parseUpdateAt);
+
+  expect(responseBody.dependences.database.version).toEqual("16.2");
+  expect(responseBody.dependences.database.max_connections).toEqual(100);
+  expect(responseBody.dependences.database.opened_connections).toEqual(1);
+});
+
+test.only("Teste de SQL Injection", async () => {
+  // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+  // await fetch("http://localhost:3000/api/v1/status?databaseName=");
+  // await fetch("http://localhost:3000/api/v1/status?databaseName=';");
+  await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_local(4); --");
 });
